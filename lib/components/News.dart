5b7fb2328f51b3ce3eb7e6d7f71c5565b664895a
Divyesh Srivastava <srivastavadivyesh4@gmail.com>
2020-06-12 18:39:52 +0530

    url_launcher added

--- lib/components/News.dart
+++ lib/components/News.dart
@@ -1,5 +1,6 @@
+
 import 'package:flutter/material.dart';
-import 'package:practise/pages/New_details.dart';
+//import 'package:url_launcher/url_launcher.dart';
 
 class News extends StatefulWidget {
   @override
@@ -7,10 +8,33 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> {
+   
+    
+    /*void launch_url(String url)async{
+
+     const url:'http://www.nitmeghalaya.in/nitmeghalaya/students_notice';
+
+     if(await canLaunch(url)){
+       await launch(url);
+     }
+   }*/
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+
+         child: Column(
+           
+           children: <Widget>[
+
+               GestureDetector(
+                 child:Text("Student E-Notice", style:TextStyle(decoration: TextDecoration.underline, color:Colors.blue)),
+                 onTap:(){}
+               )
+
+           ],
+
+         ),      
     );
   }
 } 
